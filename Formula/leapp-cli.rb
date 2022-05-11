8c96d4f8d25cd14c19e4e5456fb4741b16bd4e4a
Eric Villa <eric.villa91@gmail.com>
2022-05-11 15:56:39 +0200

    second commit

---
+++ Formula/leapp-cli.rb
@@ -0,0 +1,22 @@
+require "language/node"
+
+  class LeappCli < Formula
+  desc "Install Leapp CLI"
+  homepage "https://leapp.cloud"
+  version "0.1.26"
+  url "https://registry.npmjs.org/@hesketh-racing/leapp-cli/-/leapp-cli-0.1.12.tgz"
+  sha256 "e8cead87724fd06e4502d30f60802a288147497e956712996d34e8bf55f5077b"
+
+  depends_on "node@16"
+  depends_on "libsecret"
+  depends_on "python" => :build
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system bin/"leapp", "version"
+  end
+end
