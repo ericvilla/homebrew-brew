f24576f087a95515385ad322152993de042fa646
Eric Villa <eric.villa91@gmail.com>
2022-05-19 10:23:14 +0200

    asd

--- Formula/leapp-cli.rb
+++ Formula/leapp-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
   class LeappCli < Formula
   desc "Install Leapp CLI"
   homepage "https://leapp.cloud"
-  version "0.1.17"
-  url "https://registry.npmjs.org/@hesketh-racing/leapp-cli/-/leapp-cli-0.1.17.tgz"
+  version "0.1.18"
+  url "https://registry.npmjs.org/@hesketh-racing/leapp-cli/-/leapp-cli-0.1.18.tgz"
   sha256 "2d933515729b2a68120750d458545b96eeb6fe8a0aa64432219569c28ef5b677"
 
   depends_on "node@16"
@@ -12,7 +12,7 @@ require "language/node"
   depends_on "python" => :build
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec).reject { |a| a == "--build-from-source" }
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
