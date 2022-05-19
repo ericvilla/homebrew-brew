1c57cf795d11d1826911bc719859dc7cb83db5cd
Eric Villa <eric.villa91@gmail.com>
2022-05-19 11:54:46 +0200

    leapp-cli v0.1.19

---
+++ Formula/leapp-cli-arm64.rb
@@ -0,0 +1,17 @@
+class LeappCli < Formula
+  desc "Install Leapp CLI"
+  homepage "https://leapp.cloud"
+  version "0.1.19"
+  url "https://noovolari-leapp-website-distribution-cli.s3.eu-west-1.amazonaws.com/0.1.19/leapp-v0.1.19-6a3ff1f5-darwin-arm64.tar.xz"
+  sha256 "19b3b4c49fa0106a3f7f470196d07d488d97ea76cd10ec6ce6dce3568316a308"
+
+  def install
+    inreplace "bin/leapp", /^CLIENT_HOME=/, "export LEAPP_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/leapp"
+  end
+
+  test do
+    system bin/"leapp", "version"
+  end
+end
